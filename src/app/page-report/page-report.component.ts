011df13f464023cabaa1e01d079ceb8607935da3
黄进 <huangjin@antiy.cn>
2018-08-22 18:05:48 +0800

    增加报表筛选功能

--- src/app/page-report/page-report.component.ts
+++ src/app/page-report/page-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Project, Report, Week } from '../project';
+import { Project, Report, Reporter, Week } from '../project';
 import { ReportService } from '../report.service';
 import { WeekService } from '../week.service';
 import { ProjectService } from '../project.service';
@@ -17,9 +17,77 @@ export class PageReportComponent implements OnInit, OnDestroy {
   weekOptions: Array<Week> = [];
   reports = new Map<string, Report[]>();
   projects: Project[] = [];
+  filter: Partial<{
+    project: string[];
+    user: string[];
+    team: string[];
+  }> = {};
+
+  get existedProjectList(): Project[] {
+    return Array.from(this.reports.keys())
+      .map(key => this.projects.find(p => p.id === key))
+      .filter(p => !!p)
+      .sort((a, b) => {
+        if (a.id > b.id) {
+          return 1;
+        }
+        if (a.id < b.id) {
+          return -1;
+        }
+        return 0;
+      });
+  }
+  get teamList(): string[] {
+    return Array.from(
+      new Set(
+        Array.from(this.reports.values())
+          .reduce((last, curr) => {
+            last = last.concat(...curr);
+            return last;
+          }, [])
+          .map(c => c.team)
+      )
+    ).sort();
+  }
+  get userList(): string[] {
+    return Array.from(
+      new Set(
+        Array.from(this.reports.values())
+          .reduce((last, curr) => {
+            last = last.concat(...curr);
+            return last;
+          }, [])
+          .map(c => c.reporter)
+      )
+    ).sort();
+  }
 
   get _reports(): Report[][] {
-    const ret = Array.from(this.reports.values());
+    let ret = Array.from(this.reports.values());
+    // 按项目过滤
+    if (this.filter.project && this.filter.project.length > 0) {
+      ret = ret.filter(
+        r =>
+          r.length > 0 &&
+          this.filter.project.findIndex(p => p === r[0].project) > -1
+      );
+    }
+    // 按小组过滤
+    if (this.filter.team && this.filter.team.length > 0) {
+      ret = ret
+        .map(r => {
+          return r.filter(x => this.filter.team.indexOf(x.team) > -1);
+        })
+        .filter(r => r.length > 0);
+    }
+    // 按人员过滤
+    if (this.filter.user && this.filter.user.length > 0) {
+      ret = ret
+        .map(r => {
+          return r.filter(x => this.filter.user.indexOf(x.reporter) > -1);
+        })
+        .filter(r => r.length > 0);
+    }
     ret.sort((a, b) => {
       if (a.length > 0 && b.length > 0) {
         if (a[0].project > b[0].project) {
